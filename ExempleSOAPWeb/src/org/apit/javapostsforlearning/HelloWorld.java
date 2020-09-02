c9fb2e93117eca37794d2eb8de9fc83e270428a7
Moez Baklouti <Moez Baklouti@DESKTOP-F92A820>
2020-09-02 12:38:19 +0200

    First Commit

---
+++ ExempleSOAPWeb/src/org/apit/javapostsforlearning/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.apit.javapostsforlearning;
+
+/**
+ * TODO: Document me!
+ *
+ * @author Moez Baklouti
+ *
+ */
+public class HelloWorld {
+    public String sayHelloWorld(String name)
+    {
+    return "Hello world from "+ name;
+    }
+
+}
