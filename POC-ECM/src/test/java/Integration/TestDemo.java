0fdedae602e77f77ae64c07944d961f485e75377
moez baklouti <moezmansour.baklouti@esprit.tn>
2020-09-08 12:55:39 +0200

    Commit

---
+++ POC-ECM/src/test/java/Integration/TestDemo.java
@@ -0,0 +1,15 @@
+package Integration;
+import org.testng.annotations.*;
+/**
+ * TODO: Document me!
+ *
+ * @author Moez Baklouti
+ *
+ */
+public class TestDemo {
+    @Test
+    public void PrintSel(){
+        System.out.println("First   Build from Design Studio");
+    }
+
+}
